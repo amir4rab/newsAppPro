2fa0bdd9254f944cb3b50a9a505be14991bc83a7
amir4rab <60965542+amir4rab@users.noreply.github.com>
2020-10-14 17:52:09 +0330

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NewsManagerComponent } from './news-manager/news-manager.component';
+import { NavbarTopComponent } from './navbar-top/navbar-top.component';
+import { NavbarSecComponent } from './navbar-sec/navbar-sec.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewsManagerComponent,
+    NavbarTopComponent,
+    NavbarSecComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/navbar-sec/navbar-sec.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar-sec',
+  templateUrl: './navbar-sec.component.html',
+  styleUrls: ['./navbar-sec.component.scss']
+})
+export class NavbarSecComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
