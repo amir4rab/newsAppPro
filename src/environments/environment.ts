833e3ee585e76031f4431f9e94462b344ddd4df1
amir arab <60965542+amir4rab@users.noreply.github.com>
2021-04-24 18:36:07 +0430

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  openweathermapKey: "e669130f4538ca7afd5cabe4cafffc2d",
+  openweathermapKey: "d3d04a04a3ff20325bc8c4c4bb3474f1",
   newsapiKey: "171d44efc25346f4800fb87c06ea9352",
   GNews: "e42a8c259a4d777d45ea7fa0713e3daa",
   firebaseConfig: {
